4d1516696d0b9d9e83319f0fb9a96b5515883bee
juancruz12 <juancruz.centurion1@alu.unq.edu.ar>
2022-06-27 23:37:15 -0300

    refactors varios, falta que al ejecutar juego se sincronice bien el ganador

--- unq-ui-juan-cruz-centurion-trabajo-final/src/App.js
+++ unq-ui-juan-cruz-centurion-trabajo-final/src/App.js
@@ -14,7 +14,10 @@ const App = () => {
   const elementsGame = [tijera, lagarto,piedra,spock,papel]
 
   const [chosenItem, setChosenItem] = useState(interrogacion);
-  const [machineItem, setMachineItem] = useState(interrogacion)
+  const [machineItem, setMachineItem] = useState(interrogacion);
+  const [winner, setWinner] = useState("")
+  const [inGame, setInGame] = useState(true)
+
 
 
     function selectName(element) {
@@ -32,36 +35,86 @@ const App = () => {
           return "";
       }
   }
-
-
-    function setElementChoosed(elem){ 
-      setChosenItem(elem);
-    }
-
-    const executeGame = () => {
+    const computerPlay = () => {
+      
+      
       setMachineItem(elementsGame[Math.floor(Math.random()*elementsGame.length)])
+      compareElements()
     }
+
+    const compareElements = () => {
+      let winner = ""
+      winner = setWinner(selectWinner(selectName(chosenItem), selectName(machineItem)))
+    }
+
+    const selectWinner = (elementPlayer, elementMachine) =>{
+      console.log(elementPlayer)
+      console.log(elementMachine)
+
+      setInGame(false)
+
+      if (elementPlayer == "TIJERA" && elementMachine == "PAPEL"){
+        console.log("sdasdasd")
+        return  "JUGADOR"
+      }else if (elementPlayer == "PAPEL" && elementMachine == "PIEDRA"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "PIEDRA" && elementMachine == "LAGARTO"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "LAGARTO" && elementMachine == "SPOCK"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "SPOCK" && elementMachine == "TIJERA"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "TIJERA" && elementMachine == "LAGARTO"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "PAPEL" && elementMachine == "PIEDRA"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "LAGARTO" && elementMachine == "PAPEL"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "PAPEL" && elementMachine == "SPOCK"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "SPOCK" && elementMachine == "PIEDRA"){
+        return  "JUGADOR"
+      }else if (elementPlayer == "PIEDRA" && elementMachine == "TIJERA"){
+        return  "JUGADOR"
+      }
+
+
+    }
+
+    const reset = () => {
+      setChosenItem(interrogacion)
+      setMachineItem(interrogacion)
+      setWinner("")
+      setInGame(true)
+    }
+
+    useEffect(() => { 
+
+    }, [machineItem])
 
   return (
+
     
     <div class="container">
-        <div class="row row-cols-auto">
-            <div class = "col-md-5">
-              <div className="card-group">
-                <div class="row row-cols-5 row-cols-md-1 g-5">
-                  {elementsGame.map(elem => 
-                  <Element onClick = {() => setElementChoosed(elem)} element= {elem} elementName = {selectName(elem)} message = "Elegir"/>)}
-                </div>
-              </div>
-            </div>
-      
-        <div class="col-md-5"> 
 
+        <div>
+            <div className="card-group">
+                  {elementsGame.map(elem => (
+                      <Element onClick = {() => setChosenItem(elem)} element= {elem} elementName = {selectName(elem) } message = "Elegir" disabled = {inGame}/>
+                  ))}
+            </div>
+        </div>
+
+        <div class="row row-cols-auto">
+            
           <Element element= {chosenItem} elementName = {`Has elegido: ${selectName(chosenItem)}`} />
 
           <div class="col-md-5">    
             <div class="text-center">
                   <h1>VERSUS</h1>
+            </div>
+            <div class="text-center">
+                  <h1>{winner}</h1>
             </div>
           </div>
 
@@ -69,10 +122,10 @@ const App = () => {
 
           <Element element= {machineItem} elementName = {`La maquina ha elegido: ${selectName(machineItem)}`} />
 
-          <button onClick={() => executeGame()} class="btn btn-primary" type="button">Ejecutar juego!</button>
+          <button onClick={() => computerPlay()} class="btn btn-primary" type="button">Ejecutar juego!</button>
+          <button onClick={() => reset()} class="btn btn-primary" type="button">Reset</button>
       
 
-        </div>
 
 
         </div>
--- unq-ui-juan-cruz-centurion-trabajo-final/src/Element.js
+++ unq-ui-juan-cruz-centurion-trabajo-final/src/Element.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 import React from 'react';
 
 
-const Element = ({ element, onClick, elementName, message }) => {
+const Element = ({ element, onClick, elementName, message, disabled }) => {
+
+    
 
 
     return(
         
                  <div class="col">
-                     <div class="w-50 p-3">
-                    <div class="card text-center" >
+                     <div class="w-80 p-3">
+                    <div class="cards"  >
                     
                         <img src={element} class="card-img-top" alt="..."/>
                     
                         <h2 class="card text-center">{elementName}</h2>
-                        {message ? (<button onClick={onClick} class="btn btn-primary" type="button">{message}</button>): undefined}
+                        {message ? ( !disabled ? (<button onClick={onClick} class="btn btn-primary" type="button" disabled >{message}</button>) :
+                        <button onClick={onClick} class="btn btn-primary" type="button" >{message}</button> ): undefined}
                     </div>
                 </div>
                 </div>
